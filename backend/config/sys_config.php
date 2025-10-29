3d940757333f53762cb621ca742249e26312e4cb
Grant Du Preez <71445407+GrantODP@users.noreply.github.com>
2025-10-29 13:58:58 +0200

    Update sys_config.php

--- backend/config/sys_config.php
+++ backend/config/sys_config.php
@@ -1,17 +1,17 @@
 <?php
 $config =  [
   'database' => [
-    'host' => 'mysql-1b7f1ee8-grantoranduprez-de06.i.aivencloud.com',
-    'user' => 'avnadmin',
-    'password' => 'AVNS_pww21FkaqpGQcgixFkB',
-    'port' => 15890,
+    'host' => '',
+    'user' => '',
+    'password' => '',
+    'port' => ,
     'dbname' => 'c2c',
   ],
   'llm' =>
   [
     'gemini' =>
     [
-      'key' => 'AIzaSyA419SLbvV74Lz8_RqIwLhjexf35uws4Jg',
+      'key' => '',
       'model' => 'gemini-2.0-flash',
       'url' => "https://generativelanguage.googleapis.com/v1beta/models/MODEL_PLACE:generateContent?key=API_PLACE",
     ],
